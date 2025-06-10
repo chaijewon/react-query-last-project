1143f791a5c8decac626c10e67934676efb80bb8
chaijewon <vcandjava@nate.com>
2025-06-10 12:20:45 +0900

    1111

--- src/components/recipe/RecipeDetail.tsx
+++ src/components/recipe/RecipeDetail.tsx
@@ -57,6 +57,103 @@ function RecipeDetail() {
             <section className="archive-area section_padding_80">
                 <div className="container">
                     <div className="row">
+                        <table className={"table"}>
+                            <tbody>
+                              <tr>
+                                  <td colSpan={3} className={"text-center"}>
+                                      <img src={recipe?.vo.poster} alt={""} style={{"width":"800px","height":"250px"}} />
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td colSpan={3} className={"text-center"}>
+                                      <h3>{recipe?.vo.title}</h3>
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td colSpan={3} className={"text-center"}>
+                                      {recipe?.vo.content}
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td className={"text-center"}><img src={"/icon/a1.png"} alt={""}/></td>
+                                  <td className={"text-center"}><img src={"/icon/a2.png"} alt={""}/></td>
+                                  <td className={"text-center"}><img src={"/icon/a3.png"} alt={""}/></td>
+                              </tr>
+                              <tr>
+                                  <td className={"text-center"}>{recipe?.vo.info1}</td>
+                                  <td className={"text-center"}>{recipe?.vo.info2}</td>
+                                  <td className={"text-center"}>{recipe?.vo.info3}</td>
+                              </tr>
+                            </tbody>
+                        </table>
+                        <table className={"table"}>
+                            <tbody>
+                              <tr>
+                                  <td><h3>[재료]</h3></td>
+                              </tr>
+                              <tr>
+                                  <td>
+                                      <ul>
+                                          {
+                                              recipe &&
+                                              recipe?.dList.map((datas:string) => (
+                                                  <li>{datas}</li>
+                                              ))
+                                          }
+                                      </ul>
+                                  </td>
+                              </tr>
+                            </tbody>
+                        </table>
+                        <table className={"table"}>
+                            <tbody>
+                              <tr>
+                                  <td><h3>[조리순서]</h3></td>
+                              </tr>
+                              <tr>
+                                  <td>
+                                      {
+                                          recipe &&
+                                          recipe?.mList.map((fm:string,index:number) =>
+                                            <table className={"table"}>
+                                                <tbody>
+                                                 <tr>
+                                                     <td className={"text-left"} width={"80%"}>
+                                                         {fm}
+                                                     </td>
+                                                     <td className={"text-right"} width={"20%"}>
+                                                         <img src={recipe.iList[index]} alt={""} style={{"width":"100px","height":"80px"}}/>
+                                                     </td>
+                                                 </tr>
+                                                </tbody>
+                                            </table>
+                                          )
+                                      }
+                                  </td>
+                              </tr>
+                            </tbody>
+                        </table>
+                        <table className={"table"}>
+                            <tr>
+                                <td colSpan={2}><h3>레시피 작성자</h3></td>
+                            </tr>
+                            <tr>
+                                <td width={"30%"} className={"text-center"}>
+                                    <img src={recipe?.vo.chef_poster} alt={""} style={{"width":"100px","height":"80px"}}/>
+                                </td>
+                                <td width={"70%"}>
+                                    <h3>{recipe?.vo.chef}</h3><br/>
+                                    {recipe?.vo.chef_profile}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2} className={"text-right"}>
+                                    <button className={"btn-sm btn-danger"}
+                                     onClick={()=>nav(-1)}
+                                    >목록</button>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </section>
